68146ee22885429358ac7ec465dbd62c52cebf67
caraclarke <cara.clarke8@gmail.com>
2015-11-30 15:04:45 -0500

    Beginning eye animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function(){
+$(document).ready(function(){
 
   var documentEl = $(document),
       parallaxBg = $('div.parallax-bg');
@@ -8,4 +8,14 @@
       parallaxBg.css('background-position', '0 ' + -currScrollPos/4 + 'px');
   });
 
-})();
+  // var imageAnimation = function() {
+  //   $("#intro-image-one").animate({
+  //     left: '250px',
+  //     height: '-=100%',
+  //     width: '-=100%',
+  //     opacity: 0,
+  //   }, 10);
+  // }
+
+  // imageAnimation();
+});
